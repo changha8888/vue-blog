fc3bb12f215316566d295dcb167d3b8f3c10fe7d
changha8888 <hachang@mz.co.kr>
2021-07-09 17:06:47 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Navigation />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navigation from '@/components/Navigation.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Navigation
+  }
+}
+</script>
